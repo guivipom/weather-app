9beae347d273a8f13adbbcf758dbdc63c1b4368b
guivipom <guivipom@gmail.com>
2020-08-24 21:00:51 +0200

    Add error handlers for API request

--- app.js
+++ app.js
@@ -9,13 +9,22 @@ request({url: urlWeather, json: true}, (error,response)=>{
     if(error){
         console.log('Unable to connect to weather service!')
 
-    } else {
+    } else if (response.body.error) {
+        console.log('PUnable to find location')
+    }
+    else {
     console.log('Its currently '+response.body.current.temperature+' degrees outside and the proability of rain is: '+response.body.current.precip*100 +'%' )
     }
 })
 
 request ({url: urlMap, json: true}, (error, response) => {
-    const latitude = response.body.features[0].center[1]
-    const longitude = response.body.features[0].center[0]
-    console.log(latitude, longitude)
+    if (error) {
+        console.log('Unable to connect to location services!')
+        } else if (response.body.features.length === 0) {
+        console.log('Unable to find location. Try another search.')
+        } else {
+        const latitude = response.body.features[0].center[1]
+        const longitude = response.body.features[0].center[0]
+        console.log(latitude, longitude)
+    }
 })
