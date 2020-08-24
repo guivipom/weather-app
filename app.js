55324cfd0ab6e2cdff618c80fcfabac3896cd931
guivipom <guivipom@gmail.com>
2020-08-24 14:13:31 +0200

    Add API request

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=63827da0b81ce731a2c3a79c74f8867f&query=New York'
+const url = 'http://api.weatherstack.com/current?access_key=63827da0b81ce731a2c3a79c74f8867f&query=Berlin'
 
-request({url: url},(error,response)=>{
-    console.log(response)
+request({url: url, json: true}, (error,response)=>{
+    console.log(response.body)
 })
