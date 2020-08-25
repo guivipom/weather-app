009d8ad6277800f9b35bd85f2bdb7641b2129bfa
guivipom <guivipom@gmail.com>
2020-08-25 16:16:46 +0200

    Refactor geocode to be reused, using callbacks

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const request = require('request')
 
 const urlWeather = 'http://api.weatherstack.com/current?access_key=63827da0b81ce731a2c3a79c74f8867f&query=Berlin'
 
-const urlMap = "https://api.mapbox.com/geocoding/v5/mapbox.places/berlin.json?access_token=pk.eyJ1IjoiZ3Vpdmlwb20iLCJhIjoiY2tlOGpseWV5MHd5dDJxbWl4ZzBpeWZnayJ9.SqK_vCAh-h2ccje_XQd07Q&limit=1"
-
-
 request({url: urlWeather, json: true}, (error,response)=>{
     if(error){
         console.log('Unable to connect to weather service!')
@@ -17,14 +14,30 @@ request({url: urlWeather, json: true}, (error,response)=>{
     }
 })
 
-request ({url: urlMap, json: true}, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to location services!')
-        } else if (response.body.features.length === 0) {
-        console.log('Unable to find location. Try another search.')
-        } else {
-        const latitude = response.body.features[0].center[1]
-        const longitude = response.body.features[0].center[0]
-        console.log(latitude, longitude)
-    }
-})
+const geocode = ( address, callback ) => {
+
+    const urlMap = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+ encodeURIComponent(address) +
+                    ".json?access_token=pk.eyJ1IjoiZ3Vpdmlwb20iLCJhIjoiY2tlOGpseWV5MHd5dDJxbWl4ZzBpeWZnayJ9.SqK_vCAh-h2ccje_XQd07Q&limit=1"
+
+    request ({url: urlMap, json: true}, (error, response) => {
+
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+            } else if (response.body.features.length === 0) {
+            callback( 'Unable to find location. Try another search.', undefined)
+            } else {
+             callback( undefined , {
+                latitude: response.body.features[0].center[1] ,
+                longitude: response.body.features[0].center[0] ,
+                name: response.body.features[0].place_name
+            })
+        }
+    }) 
+}
+
+
+
+geocode('New york', (error, data)=> {
+    console.log('Error', error)
+    console.log('Data', data)
+}) 
