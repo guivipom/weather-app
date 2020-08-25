1f284a37718b27ab3042ee414f3b6e517b49fadc
guivipom <guivipom@gmail.com>
2020-08-25 16:49:08 +0200

    Refactor forecast function, moved to a new file

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
-const request = require('request')
 const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const urlWeather = 'http://api.weatherstack.com/current?access_key=63827da0b81ce731a2c3a79c74f8867f&query=Berlin'
-
-request({url: urlWeather, json: true}, (error,response)=>{
-    if(error){
-        console.log('Unable to connect to weather service!')
-
-    } else if (response.body.error) {
-        console.log('PUnable to find location')
-    }
-    else {
-    console.log('Its currently '+response.body.current.temperature+' degrees outside and the proability of rain is: '+response.body.current.precip*100 +'%' )
-    }
+forecast(52.521, -13.385, (error, data) =>{
+    console.log('Error', error)
+    console.log('Data', data)
 })
 
-
-
-
-
-geocode('New york', (error, data)=> {
+geocode('Berlin', (error, data)=> {
     console.log('Error', error)
     console.log('Data', data)
 }) 
