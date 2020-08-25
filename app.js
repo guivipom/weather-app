61adefd72f9193ccf45aec5593a4d0dd46f3a201
guivipom <guivipom@gmail.com>
2020-08-25 16:20:58 +0200

    Refactor geocode, moved to utils folder

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
 const urlWeather = 'http://api.weatherstack.com/current?access_key=63827da0b81ce731a2c3a79c74f8867f&query=Berlin'
 
@@ -14,26 +15,7 @@ request({url: urlWeather, json: true}, (error,response)=>{
     }
 })
 
-const geocode = ( address, callback ) => {
 
-    const urlMap = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+ encodeURIComponent(address) +
-                    ".json?access_token=pk.eyJ1IjoiZ3Vpdmlwb20iLCJhIjoiY2tlOGpseWV5MHd5dDJxbWl4ZzBpeWZnayJ9.SqK_vCAh-h2ccje_XQd07Q&limit=1"
-
-    request ({url: urlMap, json: true}, (error, response) => {
-
-        if (error) {
-            callback('Unable to connect to location services!', undefined)
-            } else if (response.body.features.length === 0) {
-            callback( 'Unable to find location. Try another search.', undefined)
-            } else {
-             callback( undefined , {
-                latitude: response.body.features[0].center[1] ,
-                longitude: response.body.features[0].center[0] ,
-                name: response.body.features[0].place_name
-            })
-        }
-    }) 
-}
 
 
 
